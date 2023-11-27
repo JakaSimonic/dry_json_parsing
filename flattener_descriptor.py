1e5ec9dbfd31c3345c65b707f3d4d47f63f3eb84
Jaka Simonic <jaka.simonic@ebury.com>
2023-11-27 20:33:58 +0100

    fixed typing

--- flattener_descriptor.py
+++ flattener_descriptor.py
@@ -1,20 +1,16 @@
 from typing import Optional
-from flattner_mixin import CollectionRet, FlattnerMixin, FlattnerType
+from flattner_mixin import FlattnerMixin, FlattnerType
 from model.properties import Properties
-from model.single import Single
-from model.origin_of_funds_collection import OriginOfFunds
-from model.currency_collection import Currency
-
 
 
 class Flattener(FlattnerMixin):
-    def __init__(self, field_type: str):
+    def __init__(self, field_type: Optional[FlattnerType]=None):
         self.flatten_func = self.flatten_mapper(field_type)
 
     def __set_name__(self, owner, name: str):
         self.name: str = name
 
-    def __get__(self, instance, owner=None) -> CollectionRet | Optional[str]:
+    def __get__(self, instance, owner=None) -> list | Optional[str]:
         if not instance:
             raise Exception("Instance only method!")
 
